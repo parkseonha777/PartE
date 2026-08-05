db8c3293c0aeada5bf8e2238875694c94626288a
jinwoo-1205 <jinwoo2567@gmail.com>
2026-08-06 04:16:31 +0900

    정기 동기화용 배치 스크립트 추가

---
+++ DB/run_sync.bat
@@ -0,0 +1,17 @@
+@echo off
+REM Pati - scheduled sync runner
+REM Backend A (DB) - runs sync_foodsafety.py and logs output
+
+cd /d "%~dp0.."
+
+if not exist "DB\sync_logs" mkdir "DB\sync_logs"
+
+for /f "delims=" %%i in ('powershell -NoProfile -Command "Get-Date -Format yyyyMMdd_HHmmss"') do set TIMESTAMP=%%i
+
+set LOGFILE=DB\sync_logs\sync_%TIMESTAMP%.log
+
+echo Sync started: %TIMESTAMP% > "%LOGFILE%"
+
+python DB\sync_foodsafety.py >> "%LOGFILE%" 2>&1
+
+echo Sync finished >> "%LOGFILE%"
